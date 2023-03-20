a65e3b2a54cb92b82a48875d686b9e56f1e703d3
Gevorg Harutyunyan <gevharut@gmail.com>
2023-03-21 02:24:11 +0400

    Setup UI-Core
    Signed-off-by: Gevorg Harutyunyan <gevharut@gmail.com>

--- app.vue
+++ app.vue
@@ -1,9 +1,12 @@
 <template>
-  <div id="app-ui-pex-search">
+  <div class="app-ui-core" id="app-ui-pex-search">
     <span class="tw-text-primary-500"> Hello World </span>
+    <BaseButton>Hello</BaseButton>
   </div>
 </template>
 
 <script lang="ts" setup>
+  
+  import { BaseButton } from '@pexeso/ui-core/next'
 
 </script>
